5cf5bef4dd392e724bbaec22f9eb26dc855a7a5c
l8k1b <abigail.theresa.moraes@gmail.com>
2022-03-12 12:08:33 -0800

    extracted setMatchDetails method

--- src/components/Calendar/MatchService.ts
+++ src/components/Calendar/MatchService.ts
@@ -1,7 +1,7 @@
 import { Match } from './MatchHistoryCard';
 
-const getAll = () => fetch(`${process.env.REACT_APP_API_DOMAIN}/match/involves/user/1`)
-  .then((response) => response.json()).then((data) => data.map((item:Match) => ({
+function setMatchDetails(item: Match) {
+  return {
     id: item.matchID,
     result: item.result,
     startTime: item.startTime,
@@ -12,35 +12,17 @@ const getAll = () => fetch(`${process.env.REACT_APP_API_DOMAIN}/match/involves/u
     location: item.location,
     description: item.description,
     allMatchDetails: item,
-  })));
+  };
+}
+
+const getAll = () => fetch(`${process.env.REACT_APP_API_DOMAIN}/match/involves/user/1`)
+  .then((response) => response.json()).then((data) => data.map((item:Match) => setMatchDetails(item)));
 
 const getPastMatches = () => fetch(`${process.env.REACT_APP_API_DOMAIN}/match/history/involves/user/1`)
-  .then((response) => response.json()).then((data) => data.map((item:Match) => ({
-    id: item.matchID,
-    result: item.result,
-    startTime: item.startTime,
-    endTime: item.endTime,
-    duration: item.duration,
-    type: item.type,
-    name: item.name,
-    location: item.location,
-    description: item.description,
-    allMatchDetails: item,
-  })));
+  .then((response) => response.json()).then((data) => data.map((item:Match) => setMatchDetails(item)));
 
 const getMatchInformationByMatchID = (id:number) => fetch(`${process.env.REACT_APP_API_DOMAIN}/match/${id}`)
-  .then((response) => response.json()).then((data) => data.map((item:Match) => ({
-    id: item.matchID,
-    result: item.result,
-    startTime: item.startTime,
-    endTime: item.endTime,
-    duration: item.duration,
-    type: item.type,
-    name: item.name,
-    location: item.location,
-    description: item.description,
-    allMatchDetails: item,
-  })));
+  .then((response) => response.json()).then((data) => data.map((item:Match) => setMatchDetails(item)));
 
 const MatchService = {
   getAll, getPastMatches, getMatchInformationByMatchID,
