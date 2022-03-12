77ff917df8634ede29d2ebe72f06d1987c169baa
Jk Local <jacky.zheng27@gmail.com>
2022-03-12 15:40:07 -0800

    feat: Added Firebase Authentication with Google/Facebook Login

--- src/login/GoogleAuth.tsx
+++ src/login/GoogleAuth.tsx
@@ -1,10 +1,26 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable react/button-has-type */
 import React from 'react';
 import styled from '@emotion/styled';
 import { useAtom } from 'jotai';
-import { GoogleLogin } from 'react-google-login';
-import { useNavigate } from 'react-router-dom';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import firebase from 'firebase/compat/app';
 import { loginDataAtom } from '../atoms/userAtom';
+import 'firebase/compat/auth';
+
+const baseURL = `${process.env.REACT_APP_API_DOMAIN}/api`;
+
+// Configure Firebase.
+const config = {
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAINGSENDERID,
+  appId: process.env.REACT_APP_FIREBASE_APPID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTID,
+};
+firebase.initializeApp(config);
 
 export const PageContainer = styled.div`
   width: 100vw;
@@ -41,7 +57,7 @@ const MediumText = styled.h1`
   font-size: 1.5rem;
 `;
 
-const SmallText = styled.h1`
+const SmallText = styled.h1`  
   font-size: 0.95rem;
 `;
 
@@ -58,42 +74,35 @@ const GoogleLogo = styled.img`
 `;
 
 function GoogleAuth() {
-  const navigate = useNavigate();
   const [loginData, setLoginData] = useAtom(loginDataAtom);
 
-  const handleFailure = (result: any) => {
-    console.log(result);
-  };
-
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const handleLogin = async (googleData: any) => {
-    // console.log('GoogleData', googleData);
-    // // TODO: POST api to backend for verification
-    // const res = await fetch('http://localhost:8080/api/googleTokenId', {
-    //   method: 'POST',
-    //   body: JSON.stringify({
-    //     tokenId: googleData.tokenId,
-    //   }),
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    //   }
-    // });
-    // const data = await res.json()
-    // console.log("Data is", data, typeof(data))
-    // if (data.googleId) {
-    //   setLoginData(data);
-    //   localStorage.setItem('loginData', JSON.stringify(data));
-    //   console.log("Data is", data)
-
-    // } else {
-    //   console.log("handleLogin: Login is not successful...")
-    // }
-    navigate('/signup');
-  };
-
-  const handleLogout = () => {
-    localStorage.removeItem('loginData');
-    setLoginData(null);
+  // Configure FirebaseUI.
+  const uiConfig = {
+  // Popup signin flow rather than redirect flow.
+    signInFlow: 'popup',
+    // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
+    signInSuccessUrl: '/dashboard',
+    // We will display Google and Facebook as auth providers.
+    signInOptions: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+    ],
+    callbacks: {
+      signInSuccessWithAuthResult(authResult : any, redirectUrl: any) {
+      // Note: the JWT Token returned from authResult has a different aud, use the one returned by getIdToken()
+        firebase.auth().currentUser?.getIdToken()
+          .then((idToken) => fetch(`${baseURL}/verifyIdToken`, {
+            method: 'POST',
+            body: idToken,
+          }))
+          .then((res) => res.json())
+          .then((data) => {
+            setLoginData(data);
+          })
+          .catch((err) => console.log(err));
+        return true;
+      },
+    },
   };
 
   return (
@@ -112,62 +121,10 @@ function GoogleAuth() {
           />
         </SubContentContainer>
         <SubContentContainer>
-          {loginData ? (
-            <div>
-              <h3>
-                {' '}
-                you logged in as
-                {' '}
-                {loginData.email}
-              </h3>
-              <button onClick={handleLogout}> Logout </button>
-            </div>
-          ) : (
-            <div className="flex flex-col h-1/2 justify-around items-center">
-              <BigText className="text-white"> Welcome back</BigText>
-              <GoogleLogin
-                clientId={
-                  process.env.REACT_APP_GOOGLE_CLIENT_ID
-                    ? process.env.REACT_APP_GOOGLE_CLIENT_ID
-                    : ''
-                }
-                render={(renderProps) => (
-                  <CustomButton
-                    className="flex items-center"
-                    onClick={renderProps.onClick}
-                    disabled={renderProps.disabled}
-                  >
-                    <GoogleLogo
-                      className="mx-3"
-                      alt="Googlelogo"
-                      // eslint-disable-next-line global-require
-                      src={require('../image/googleLogo.png')}
-                    />
-                    <SmallText style={{ color: '#444242' }}>
-                      Sign in with Google
-                      {' '}
-                    </SmallText>
-                  </CustomButton>
-                )}
-                buttonText="Login with Google"
-                onSuccess={handleLogin}
-                onFailure={handleFailure}
-                cookiePolicy="single_host_origin"
-              />
-              <MediumText className="text-white">
-                {' '}
-                New to GameSetMatch?
-                {' '}
-              </MediumText>
-              <CustomButton>
-                <SmallText style={{ color: '#444242' }}>
-                  {' '}
-                  Create a new account
-                  {' '}
-                </SmallText>
-              </CustomButton>
-            </div>
-          )}
+          <div className="flex flex-col h-1/2 justify-around items-center">
+            <BigText className="text-white"> Welcome back</BigText>
+            <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+          </div>
         </SubContentContainer>
       </ContentContainer>
       <SpaceContainer className="flex items-center justify-center">
