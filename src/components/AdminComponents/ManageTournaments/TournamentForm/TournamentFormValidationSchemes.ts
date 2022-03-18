cb580f510bb9794ebb85f6a006620e71420a30c2
gabouryn <ngaboury@gmail.com>
2022-03-18 15:32:15 -0700

    remove validation for field that is now select

--- src/components/AdminComponents/ManageTournaments/TournamentForm/TournamentFormValidationSchemes.ts
+++ src/components/AdminComponents/ManageTournaments/TournamentForm/TournamentFormValidationSchemes.ts
@@ -52,10 +52,6 @@ export const validationSchemaEdit = yup.object({
     .number()
     .min(1, 'Must be greater than 0')
     .required('Match Duration is required'),
-  numberOfMatches: yup
-    .number()
-    .min(1, 'Must be greater than 0')
-    .required('Number of matches is required'),
   startDate: yup
     .date(),
   closeRegistrationDate: yup
