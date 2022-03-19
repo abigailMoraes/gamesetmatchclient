5fe1dcb2f3f1eaf66f6e1031e1a38c46f01c50f0
abigailMoraes <98375256+abigailMoraes@users.noreply.github.com>
2022-03-19 02:50:41 -0700

    Update UserService.ts

--- src/components/Calendar/UserService.ts
+++ src/components/Calendar/UserService.ts
@@ -11,7 +11,7 @@ function setUserDetails(item: User) {
   };
 }
 
-const getMatchParticipants = (id: number) => fetch(`${process.env.REACT_APP_API_DOMAIN}/match/${id}/participants`)
+const getMatchParticipants = (id: number) => fetch(`${process.env.REACT_APP_API_DOMAIN}/api/match/${id}/participants`)
   .then((response) => (response.json())).then((data) => data.map((item:User) => setUserDetails(item)));
 
 const UserService = {
