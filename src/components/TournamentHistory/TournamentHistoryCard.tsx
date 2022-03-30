a14ebcc59de62ff2287381d601122703a94e22fb
l8k1b <abigail.theresa.moraes@gmail.com>
2022-03-30 00:35:44 -0700

    Added new functionality for: confirming match attendance updating match results

---
+++ src/components/TournamentHistory/TournamentHistoryCard.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import PlayerStats from './PlayerStats';
+import CompletedTournamentsGrid from './CompletedTournamentsGrid';
+
+function TournamentHistoryCard() {
+  return (
+    <div style={{ display: 'inline-flex', width: '100%' }}>
+      <div style={{ width: '80%' }}>
+        <PlayerStats />
+      </div>
+      <div style={{ width: '30%' }}>
+        <CompletedTournamentsGrid />
+      </div>
+    </div>
+  );
+}
+
+export default TournamentHistoryCard;
