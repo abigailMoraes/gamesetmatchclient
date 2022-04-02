aa71670b7b7f74c9c7ec42078b11c95535596631
abigailMoraes <98375256+abigailMoraes@users.noreply.github.com>
2022-04-01 22:17:08 -0700

    Update BadgesGrid.tsx

--- src/components/TournamentHistory/BadgesGrid.tsx
+++ src/components/TournamentHistory/BadgesGrid.tsx
@@ -19,7 +19,7 @@ function BadgesGrid() {
   const [tournaments, setTournaments] = useState<Tournament[]>([]);
   const [matches, setMatches] = useState<Match[]>([]);
   const [matchWins, setMatchWins] = useState<Match[]>([]);
-  const userID = 3;
+  const userID = useAtomValue(userIDAtom);
 
   useEffect(() => {
     MatchService.getPastMatches(userID).then((data) => {
