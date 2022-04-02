8093c9d075b4fc19ff00f0d99f8b86bcaa6caaea
gabouryn <ngaboury@gmail.com>
2022-04-02 11:21:04 -0700

    fix: status modal not closing and match time lt 9am or gt 9pm

--- src/components/AdminComponents/ManageTournaments/TournamentGrid/GridCardManageSchedule.tsx
+++ src/components/AdminComponents/ManageTournaments/TournamentGrid/GridCardManageSchedule.tsx
@@ -36,46 +36,52 @@ function GridCardManageSchedule({
   tournament, tournamentRows, setTournamentRows, formTournament, setFormTournament,
 }:GridCardManageScheduleProps) {
   const [open, setOpen] = React.useState(false);
-  const [errorModal, setErrorModal] = React.useState(false);
+  const [error, setError] = React.useState(false);
   // TODO enable delete if its the first round only
   const [matches, setMatches] = React.useState<MatchForAdmin[]>([]);
   const [loading, setLoading] = React.useState(false);
   const [openStatusModal, setOpenStatusModal] = React.useState(false);
-  const [createError, setCreateError] = React.useState(false);
+  const [statusModalText, setStatusModalText] = React.useState('');
   const [schedulePublished, setSchedulePublished] = React.useState(tournament.status === TournamentStatus.RegistrationClosed);
   const [scheduleCreated, setScheduleCreated] = React.useState(tournament.status === TournamentStatus.ReadyToPublishNextRound
     || tournament.status === TournamentStatus.ReadyToPublishSchedule);
   const [enableDelete] = React.useState(canDelete(tournament));
-  const [errorMessage, setErrorMessage] = React.useState('');
 
   const tooltip1 = "A schedule has already been created. Press 'Publish Schedule' to view.";
   const tooltip2 = 'Please create a schedule first.';
 
   const openPublishSchedule = () => {
-    setErrorModal(false);
+    setError(false);
     ManageTournamentService.getLatestRoundID(tournament.tournamentID)
-      .then((roundID:number) => ManageTournamentService.getMatchesNeedingScheduling(roundID))
+      .then((roundID:number) => {
+        if (roundID) {
+          return ManageTournamentService.getMatchesNeedingScheduling(roundID);
+        }
+        throw new Error('No schedule found. Round number is invalid.');
+      })
       .then((data:MatchForAdmin[]) => {
         setMatches(data);
         setOpen(true);
       }).catch((err:Error) => {
-        setErrorMessage(err.message);
-        setErrorModal(true);
+        setStatusModalText(err.message);
+        setError(true);
+        setOpenStatusModal(true);
       });
   };
 
   const createSchedule = () => {
-    setCreateError(false);
+    setError(false);
     setLoading(true);
     ManageTournamentService.createSchedule(tournament.tournamentID).then(() => {
       setLoading(false);
+      setStatusModalText("Schedule was successfully created, click 'Publish Schedule' to view.");
       setOpenStatusModal(true);
       setScheduleCreated(true);
       setSchedulePublished(false);
     }).catch((err:Error) => {
-      setErrorMessage(err.message);
+      setStatusModalText(err.message);
       setLoading(false);
-      setCreateError(true);
+      setError(true);
       setOpenStatusModal(true);
     });
   };
@@ -116,19 +122,11 @@ function GridCardManageSchedule({
         enableEdit
       />
       <StatusModal
-        open={errorModal}
-        handleDialogClose={handleDialogClose}
-        dialogText="There was an error with retrieving the schedule.Please try again later or contact support."
-        dialogTitle="Error"
-        isError
-      />
-      <StatusModal
         open={openStatusModal}
         handleDialogClose={handleDialogClose}
-        dialogText={createError ? errorMessage
-          : "Schedule was successfully created, click 'Publish Schedule' to view"}
-        dialogTitle={createError ? 'Error' : 'Success'}
-        isError={createError}
+        dialogText={statusModalText}
+        dialogTitle={error ? 'Error' : 'Success'}
+        isError={error}
       />
       <LoadingOverlay isOpen={loading} />
     </>
