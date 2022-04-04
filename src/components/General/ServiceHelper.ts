6172fc4fd9ebfcd6cec462f5798610a55acdcd10
gabouryn <ngaboury@gmail.com>
2022-04-04 13:57:21 -0700

    remove console logs

--- src/components/General/ServiceHelper.ts
+++ src/components/General/ServiceHelper.ts
@@ -1,8 +1,6 @@
 export default function handleErrors(response:any) {
   if (!response.ok) {
-    console.log(`response:${response}`);
     return response.json().then((text:any) => {
-      console.log(text);
       if (text.message) {
         // TODO type this error
         throw new Error(text.message);
